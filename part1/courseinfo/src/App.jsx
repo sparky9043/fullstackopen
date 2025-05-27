01e40ebfdc119dedce73c56ad6c38dbb290dfaad
Se Joon Park <sparky9043@gmail.com>
2025-05-27 17:11:52 +0900

    Create and pass data into content component and call it in app

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,5 +1,22 @@
 const Header = (props) => <h1>{props.course}</h1>;
 
+const Content = (props) => {
+  console.log(props);
+  return (
+    <>
+      <p>
+        {props.parts[0]} {props.exercises[0]}
+      </p>
+      <p>
+        {props.parts[1]} {props.exercises[1]}
+      </p>
+      <p>
+        {props.parts[2]} {props.exercises[2]}
+      </p>
+    </>
+  );
+};
+
 const App = () => {
   const course = "Half Stack application development";
   const part1 = "Fundamentals of React";
@@ -12,15 +29,10 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
+      <Content
+        parts={[part1, part2, part3]}
+        exercises={[exercises1, exercises2, exercises3]}
+      />
       <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
     </div>
   );
