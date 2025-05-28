26ff121fa1b4f3f73dc10a972210d116b060b2d6
Se Joon Park <sparky9043@gmail.com>
2025-05-28 17:38:32 +0900

    Change code to update regardless of how many parts there are

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -18,9 +18,9 @@ const Header = (props) => <h1>{props.course}</h1>;
 
 const Content = (props) => (
   <div>
-    <Part part={props.parts[0]} />
-    <Part part={props.parts[1]} />
-    <Part part={props.parts[2]} />
+    {props.parts.map((part) => (
+      <Part part={part} key={part.name} />
+    ))}
   </div>
 );
 
