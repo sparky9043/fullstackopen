ef8a6998fdcdb4664a24e350fee1689910cc95e0
Se Joon Park <sparky9043@gmail.com>
2025-05-28 15:43:44 +0900

    Update derived state to calculate total

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -14,6 +14,8 @@ const App = () => {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
+  const total = good + neutral + bad;
+
   function handleGood() {
     setGood((good) => good + 1);
   }
@@ -36,6 +38,7 @@ const App = () => {
       <Stats text="good" stat={good} />
       <Stats text="neutral" stat={neutral} />
       <Stats text="bad" stat={bad} />
+      <Stats text="all" stat={total} />
     </div>
   );
 };
