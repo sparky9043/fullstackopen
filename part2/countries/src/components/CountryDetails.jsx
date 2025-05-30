5350e857d7e19c2a14d89bc287d329e92687a2b5
Se Joon Park <sparky9043@gmail.com>
2025-05-30 15:44:45 +0900

    Make country detail page show flag and languages

--- part2/countries/src/components/CountryDetails.jsx
+++ part2/countries/src/components/CountryDetails.jsx
@@ -1,10 +1,17 @@
 const CountryDetails = ({ country }) => {
-  console.log(country);
+  const languages = [...Object.values(country.languages)];
   return (
     <div>
-      <h2>{country.name.common}</h2>
+      <h1>{country.name.common}</h1>
       <p>Capital {country.capital.join(", ")}</p>
       <p>Area {country.area}</p>
+      <h2>Languages</h2>
+      <ul>
+        {languages.map((language) => (
+          <li key={language}>{language}</li>
+        ))}
+      </ul>
+      <img src={country.flags.png} />
     </div>
   );
 };
