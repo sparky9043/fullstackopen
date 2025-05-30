d1708b37b0c83ddc2c3e9d6fb52ee88d038a3077
Se Joon Park <sparky9043@gmail.com>
2025-05-30 11:19:22 +0900

    Use axios to fetch and log data to console

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -1,4 +1,21 @@
+import { useEffect, useState } from "react";
+import countriesServices from "./services/countries";
+
 const App = () => {
+  const [allCountries, setAllCountries] = useState(null);
+
+  useEffect(() => {
+    const countriesData = countriesServices.getAll();
+
+    countriesData.then((initialData) => setAllCountries(initialData));
+  }, []);
+
+  if (!allCountries) {
+    return null;
+  }
+
+  console.log(allCountries.length);
+
   return <div>Hello</div>;
 };
 
--- part2/countries/src/services/countries.js
+++ part2/countries/src/services/countries.js
@@ -3,10 +3,7 @@ import axios from "axios";
 const baseUrl = "https://studies.cs.helsinki.fi/restcountries/api";
 
 const getAll = async () => {
-  return axios.get(`${baseUrl}/all`).then((response) => {
-    console.log(response.data);
-    return response.data;
-  });
+  return axios.get(`${baseUrl}/all`).then((response) => response.data);
 };
 
 export default {
