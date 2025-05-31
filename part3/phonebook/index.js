958a89030c2f936b5cf493aa8e72350e106862ea
Se Joon Park <sparky9043@gmail.com>
2025-05-31 17:12:42 +0900

    Add get request handling for specific person with ids

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -36,6 +36,17 @@ app.get("/info", (request, response) => {
     <p>${getCurrentTime()}</p>`);
 });
 
+app.get("/api/persons/:id", (request, response) => {
+  const id = request.params.id;
+  const person = persons.find((p) => p.id === id);
+
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
+});
+
 const PORT = 3001;
 
 app.listen(PORT, () => {
