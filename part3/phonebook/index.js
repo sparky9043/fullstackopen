dd4384ff0c62e96932fc7b50586fb043b9122611
Se Joon Park <sparky9043@gmail.com>
2025-06-03 16:16:54 +0900

    Comment request functions

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
@@ -8,28 +9,28 @@ app.use(express.json());
 app.use(express.static("dist"));
 app.use(cors());
 
-let persons = [
-  {
-    id: "1",
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: "2",
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: "3",
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: "4",
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
+// let persons = [
+//   {
+//     id: "1",
+//     name: "Arto Hellas",
+//     number: "040-123456",
+//   },
+//   {
+//     id: "2",
+//     name: "Ada Lovelace",
+//     number: "39-44-5323523",
+//   },
+//   {
+//     id: "3",
+//     name: "Dan Abramov",
+//     number: "12-43-234345",
+//   },
+//   {
+//     id: "4",
+//     name: "Mary Poppendieck",
+//     number: "39-23-6423122",
+//   },
+// ];
 
 app.use(
   morgan(function (tokens, req, res) {
@@ -46,80 +47,80 @@ app.use(
   })
 );
 
-app.get("/api/persons", (request, response) => {
-  response.json(persons);
-});
+// app.get("/api/persons", (request, response) => {
+//   response.json(persons);
+// });
 
-const getCurrentTime = () => new Date();
+// const getCurrentTime = () => new Date();
 
-app.get("/info", (request, response) => {
-  response.send(`<p>Phonebook has info for ${persons.length} people</p>
-    <p>${getCurrentTime()}</p>`);
-});
+// app.get("/info", (request, response) => {
+//   response.send(`<p>Phonebook has info for ${persons.length} people</p>
+//     <p>${getCurrentTime()}</p>`);
+// });
 
-app.get("/api/persons/:id", (request, response) => {
-  const id = request.params.id;
-  const person = persons.find((p) => p.id === id);
+// app.get("/api/persons/:id", (request, response) => {
+//   const id = request.params.id;
+//   const person = persons.find((p) => p.id === id);
 
-  if (person) {
-    response.json(person);
-  } else {
-    response.status(404).end();
-  }
-});
+//   if (person) {
+//     response.json(person);
+//   } else {
+//     response.status(404).end();
+//   }
+// });
 
-app.delete("/api/persons/:id", (request, response) => {
-  const id = request.params.id;
-  persons = persons.filter((p) => p.id !== id);
+// app.delete("/api/persons/:id", (request, response) => {
+//   const id = request.params.id;
+//   persons = persons.filter((p) => p.id !== id);
 
-  response.status(204).end();
-});
+//   response.status(204).end();
+// });
 
-const generateId = () => {
-  const random1 = Math.floor(Math.random() * 50);
-  const random2 = Math.floor(Math.random() * 50) + 50;
-  const newId = random1 * random2;
+// const generateId = () => {
+//   const random1 = Math.floor(Math.random() * 50);
+//   const random2 = Math.floor(Math.random() * 50) + 50;
+//   const newId = random1 * random2;
 
-  return String(newId);
-};
+//   return String(newId);
+// };
 
-const postErrorMessages = {
-  repeatName: "name must be unique",
-  emptyName: "did not provide a name",
-  emptyPhone: "did not provide phone number",
-};
+// const postErrorMessages = {
+//   repeatName: "name must be unique",
+//   emptyName: "did not provide a name",
+//   emptyPhone: "did not provide phone number",
+// };
 
-app.post("/api/persons", (request, response) => {
-  const body = request.body;
+// app.post("/api/persons", (request, response) => {
+//   const body = request.body;
 
-  const matching = persons.find(
-    (person) => person.name.toLowerCase() === body.name.toLowerCase()
-  );
+//   const matching = persons.find(
+//     (person) => person.name.toLowerCase() === body.name.toLowerCase()
+//   );
 
-  if (matching) {
-    response.status(400).json({
-      error: postErrorMessages.repeatName,
-    });
-  } else if (!body.name) {
-    response.status(400).json({
-      error: postErrorMessages.emptyName,
-    });
-  } else if (!body.number) {
-    response.status(400).json({
-      error: postErrorMessages.emptyPhone,
-    });
-  }
+//   if (matching) {
+//     response.status(400).json({
+//       error: postErrorMessages.repeatName,
+//     });
+//   } else if (!body.name) {
+//     response.status(400).json({
+//       error: postErrorMessages.emptyName,
+//     });
+//   } else if (!body.number) {
+//     response.status(400).json({
+//       error: postErrorMessages.emptyPhone,
+//     });
+//   }
 
-  const newPerson = {
-    name: body.name,
-    number: body.number,
-    id: generateId(),
-  };
+//   const newPerson = {
+//     name: body.name,
+//     number: body.number,
+//     id: generateId(),
+//   };
 
-  persons = persons.concat(newPerson);
+//   persons = persons.concat(newPerson);
 
-  response.json(newPerson);
-});
+//   response.json(newPerson);
+// });
 
 const PORT = process.env.PORT || 3001;
 
