df7c89cd158c240f8421d59d53e5322e55411f95
Se Joon Park <sparky9043@gmail.com>
2025-06-05 16:34:43 +0900

    Install and configure stylistic js rules

--- part3/phonebook/eslint.config.mjs
+++ part3/phonebook/eslint.config.mjs
@@ -1,5 +1,6 @@
 import globals from 'globals'
 import js from '@eslint/js'
+import stylisticJs from '@stylistic/eslint-plugin-js'
 
 export default [
   js.configs.recommended,
@@ -10,5 +11,14 @@ export default [
       globals: { ...globals.node },
       ecmaVersion: 'latest',
     },
+    plugins: {
+      '@stylisic/js': stylisticJs,
+    },
+    rules: {
+      '@stylistic/js/indent': ['error', 2],
+      '@stylistic/js/linebreak-style': ['error', 'unix'],
+      '@stylistic/js/quotes': ['error', 'single'],
+      '@stylistic/js/semi': ['error', 'never'],      
+    }
   },
 ]
