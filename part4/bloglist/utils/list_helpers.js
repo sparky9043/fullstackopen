d39b2b06a3e1b7732ba164eebd3663dd4d7c9733
Se Joon Park <sparky9043@gmail.com>
2025-06-06 15:41:32 +0900

    Create totalLikes function to calculate total likes

--- part4/bloglist/utils/list_helpers.js
+++ part4/bloglist/utils/list_helpers.js
@@ -2,6 +2,15 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+
+  return blogs.length === 0 ? 0 : blogs.map(blog => blog.likes).reduce((previous, current) => previous + current, 0)
+}
+
+console.log(
+  totalLikes([{ likes: 9 }, { likes: 1 }, { likes: 12 }])
+)
+
 module.exports = {
   dummy
 }
