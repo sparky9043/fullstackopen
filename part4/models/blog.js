7a036d075db949532b226287be8a05426a476b7a
Se Joon Park <sparky9043@gmail.com>
2025-06-06 13:58:01 +0900

    Refactor schema to separate model file

---
+++ part4/models/blog.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose')
+const configs = require('../utils/configs')
+
+mongoose.set('strictQuery', false)
+
+mongoose.connect(configs.MONGODB_URI)
+  .then(() => {
+    console.log('connecting to MongoDB')
+  })
+  .catch(error => {
+    console.log('Error: ', error.message)
+  })
+
+const blogSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number,
+})
+
+blogSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+module.exports = mongoose.model("Blog", blogSchema)
