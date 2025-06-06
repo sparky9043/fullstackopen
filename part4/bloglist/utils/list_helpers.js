e9320718aaa136c8f5cc4c59f6f0b3696e78d39d
Se Joon Park <sparky9043@gmail.com>
2025-06-06 16:28:30 +0900

    Make and export function that detects most blog posts by one author

--- part4/bloglist/utils/list_helpers.js
+++ part4/bloglist/utils/list_helpers.js
@@ -14,8 +14,31 @@ const favoriteBlog = (blogs) => {
   return blogs.find(blog => blog.likes === Math.max(...blogs.map(blog => blog.likes)))
 }
 
+const mostBlogs = (blogs) => {
+  if (blogs.length === 0) return undefined
+
+  const blogPosts = []
+
+  blogs.map(blog => blog.author).forEach(author => {
+    const authors = blogPosts.map(post => post.author);
+
+    if (!authors.includes(author)) {
+      blogPosts.push({
+        author, blogs: 1
+      })
+    } else {
+      blogPosts.find(post => post.author === author).blogs++
+    }
+  })
+
+  const maxBlog = blogPosts.find(post => post.blogs === Math.max(...blogPosts.map(post => post.blogs)))
+
+  return maxBlog
+}
+
 module.exports = {
   dummy,
   totalLikes,
-  favoriteBlog
+  favoriteBlog,
+  mostBlogs
 }
