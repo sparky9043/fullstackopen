884ee06831699352f47273cbb773bf246e826831
Se Joon Park <sparky9043@gmail.com>
2025-06-06 13:40:01 +0900

    Do exercise 4.1 to make code function properly

---
+++ part4/index.js
@@ -0,0 +1,45 @@
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+
+const app = express()
+
+app.use(express.json())
+
+const MONGODB_URI = process.env.MONGODB_URI
+
+mongoose.set('strictQuery', false)
+
+const blogSchema = mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number,
+})
+
+const Blog = mongoose.model("Blog", blogSchema);
+
+mongoose.connect(MONGODB_URI)
+  .then(() => {
+    console.log("connecting to MongoDB")
+  })
+
+app.get("/api/blogs", (request, response) => {
+  Blog.find({})
+    .then(blogs => {
+      response.json(blogs)
+    })
+})
+
+app.post("/api/blogs", (request, response) => {
+  const blog = new Blog(request.body)
+
+  blog.save().then((result) => {
+    response.status(201).json(result)
+  })
+})
+
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
