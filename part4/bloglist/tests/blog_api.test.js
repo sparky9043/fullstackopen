2394313bf6572c99c8b64a73018c0255a3fd3678
Se Joon Park <sparky9043@gmail.com>
2025-07-01 17:22:38 +0900

    Apply supertest to the express app and initialize

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -1,5 +1,8 @@
 const { test, after } = require('node:test')
 const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const api = supertest(app)
 
 after(async() => {
   await mongoose.connection.close()
