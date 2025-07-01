3b79463bf079c14160c28db5fef5b9ee11b2cd90
Se Joon Park <sparky9043@gmail.com>
2025-07-01 17:32:21 +0900

    Make test start with clearing blogs and adding two fixed initial notes

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -1,8 +1,32 @@
-const { test, after } = require('node:test')
+const { test, after, beforeEach } = require('node:test')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
 const api = supertest(app)
+const Blog = require('../models/blog')
+
+const initialBlogs = [
+  {
+    title: "We are the champions",
+    author: "Queen",
+    url: "http://fakeurl.com",
+    likes: 441
+  },
+  {
+    title: "I am levitating",
+    author: "Dua Lipa",
+    url: "http://thisisafakewebsite.org",
+    likes: 327
+  }
+]
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  let blogObject = new Blog(initialBlogs[0])
+  await blogObject.save()
+  blogObject = new Blog(initialBlogs[1])
+  await blogObject.save()
+})
 
 test('blogs returned as json', async () => {
   await api
