99c79d99f25d1e7184d47019942190d1f82333af
Se Joon Park <sparky9043@gmail.com>
2025-07-02 14:32:03 +0900

    Make post post respond with 404 error if missing title or author

--- part4/bloglist/controller/blogs.js
+++ part4/bloglist/controller/blogs.js
@@ -7,6 +7,12 @@ blogsRouter.get('/', async (request, response) => {
 })
 
 blogsRouter.post('/', (request, response) => {
+  const body = request.body
+
+  if (!body.title || !body.author) {
+    response.status(404).json({ error: "Missing title or author" })
+  }
+
   const blog = new Blog(request.body)
 
   blog.save().then(result => {
