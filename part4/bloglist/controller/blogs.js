a3ec215ee963596674eb2d9e8043eafbca90e831
Se Joon Park <sparky9043@gmail.com>
2025-07-07 17:48:47 +0900

    couple user and blogs to add info about each other when post request is received

--- part4/bloglist/controller/blogs.js
+++ part4/bloglist/controller/blogs.js
@@ -1,5 +1,6 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({})
@@ -9,22 +10,24 @@ blogsRouter.get('/', async (request, response) => {
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
 
-  if (!body.title || !body.url) {
-    return response
-      .status(400)
-      .json({ error: "Missing title or url" })
+  const user = await User.findById(body.userId)
+
+  if (!user) {
+    return response.status(400).json({ error: 'userId missing or not valid' })
   }
 
-  const newBlog = {
+  const blog = new Blog({
     title: body.title,
     author: body.author,
     url: body.url,
-    likes: body.likes || 0
-  }
-
-  const blog = new Blog(newBlog)
+    likes: body.likes || 0,
+    user: user._id
+  })
 
   const savedBlog = await blog.save()
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
+
   response.status(201).json(savedBlog)
 })
 
