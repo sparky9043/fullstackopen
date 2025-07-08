4ba1c1ad14c6dfa63fbaf08e02c142a9df32b549
Se Joon Park <sparky9043@gmail.com>
2025-07-08 14:44:17 +0900

    Update delete request to only work when valid user is logged in

--- part4/bloglist/controller/blogs.js
+++ part4/bloglist/controller/blogs.js
@@ -41,6 +41,22 @@ blogsRouter.post('/', async (request, response) => {
 })
 
 blogsRouter.delete('/:id', async (request, response) => {
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
+
+  if (!decodedToken.id) {
+    return response.status(401).json({
+      error: 'token invalid'
+    })
+  }
+
+  const user = await User.findById(decodedToken.id)
+
+  if (!user) {
+    return response.status(401).json({
+      error: 'user not found'
+    })
+  }
+
   await Blog.findByIdAndDelete(request.params.id)
   response.status(204).end()
 })
