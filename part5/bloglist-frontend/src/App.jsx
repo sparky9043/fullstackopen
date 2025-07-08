8c1e0603f0bfedb7c3ac8184aba14476b5f5f12b
Se Joon Park <sparky9043@gmail.com>
2025-07-08 18:39:27 +0900

    Create login form that renders when user is null

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -4,6 +4,7 @@ import blogService from './services/blogs'
 
 const App = () => {
   const [blogs, setBlogs] = useState(null)
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -17,8 +18,26 @@ const App = () => {
     )
   }
 
+  const loginForm = () => {
+    return (
+      <form>
+        <h2>log in to application</h2>
+        <div>
+          username
+          <input type="text" />
+        </div>
+        <div>
+          password
+          <input type="password" />
+        </div>
+        <button type="submit">login</button>
+      </form>
+    )
+  }
+
   return (
     <div>
+      {user === null && loginForm()}
       <h2>blogs</h2>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
