5f23a8a2b1ab52ac3eb8cd382c2d94e3cf384942
Se Joon Park <sparky9043@gmail.com>
2025-07-09 13:42:38 +0900

    display the name of user when logged in

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -73,6 +73,9 @@ const App = () => {
   return (
     <div>
       <h2>blogs</h2>
+      <p>
+        {user.name} logged in
+      </p>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
