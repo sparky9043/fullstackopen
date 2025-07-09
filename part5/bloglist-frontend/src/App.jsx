e65b872b0ea4f2b6214e1fa153f38d4843ca86fd
Se Joon Park <sparky9043@gmail.com>
2025-07-09 13:56:54 +0900

    add form to create new blog with handle function

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -8,6 +8,9 @@ const App = () => {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [url, setUrl] = useState('')
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -76,15 +79,49 @@ const App = () => {
     setUser(null)
   }
 
+  const handleCreateBlog = (event) => {
+    event.preventDefault()
+  }
+
   return (
     <div>
       <h2>blogs</h2>
-      <p>
+
+      <div>
         {user.name} logged in
         <span>
           <button onClick={handleLogout}>logout</button>
         </span>
-      </p>
+      </div>
+
+      <form onSubmit={handleCreateBlog}>
+        <h2>create new</h2>
+        <div>
+          title:
+          <input
+            type="text"
+            value={title}
+            onChange={({ target }) => setTitle(target.value)}
+          />
+        </div>
+        <div>
+          author:
+          <input
+            type="text"
+            value={author}
+            onChange={({ target }) => setAuthor(target.value)}
+          />
+        </div>
+        <div>
+          url:
+          <input
+            type="text"
+            value={url}
+            onChange={({ target }) => setUrl(target.value)}
+          />
+        </div>
+        <button type="submit">create</button>
+      </form>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
