0a1827c87b4b1e3d6bb6c838038fdcf0bc1735ae
Se Joon Park <sparky9043@gmail.com>
2025-07-09 13:46:26 +0900

    add logout button and handle logout by removing user after click

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -70,11 +70,20 @@ const App = () => {
     )
   }
 
+  function handleLogout() {
+    console.log('logged out successfully')
+    window.localStorage.removeItem('userLoginBlogApp')
+    setUser(null)
+  }
+
   return (
     <div>
       <h2>blogs</h2>
       <p>
         {user.name} logged in
+        <span>
+          <button onClick={handleLogout}>logout</button>
+        </span>
       </p>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
