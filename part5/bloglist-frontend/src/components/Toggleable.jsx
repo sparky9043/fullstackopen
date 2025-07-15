b93abd429dd4254701b6e294477cc708d5e1b916
Se Joon Park <sparky9043@gmail.com>
2025-07-15 19:32:18 +0900

    use props to make sure button label is string and required

--- part5/bloglist-frontend/src/components/Toggleable.jsx
+++ part5/bloglist-frontend/src/components/Toggleable.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import PropTypes from 'prop-types'
 
 const Toggleable = ({ children, buttonLabel = 'input' }) => {
   const [visibility, setVisibility] = useState(false)
@@ -20,6 +21,10 @@ const Toggleable = ({ children, buttonLabel = 'input' }) => {
       </div>
     </div>
   )
+}
+
+Toggleable.proptypes = {
+  buttonLabel: PropTypes.string.isRequired
 }
 
 export default Toggleable
