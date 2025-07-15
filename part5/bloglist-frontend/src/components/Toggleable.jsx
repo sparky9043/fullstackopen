511098fb6bc95d22ffa27c3dcedbb3958d97735d
Se Joon Park <sparky9043@gmail.com>
2025-07-15 15:19:39 +0900

    add basic boilerplate code to toggleable

---
+++ part5/bloglist-frontend/src/components/Toggleable.jsx
@@ -0,0 +1,15 @@
+const Toggleable = ({ children }) => {
+  return (
+    <div>
+      <div>
+
+      </div>
+      <div>
+        {children}
+      </div>
+      <button>cancel</button>
+    </div>
+  )
+}
+
+export default Toggleable
