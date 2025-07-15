274b0702a863ef27638b2749e2ee47f84bef0d69
Se Joon Park <sparky9043@gmail.com>
2025-07-15 16:04:26 +0900

    make each blog toggleable data for url and likes

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,30 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [viewDetails, setViewDetails] = useState(false) 
+  
+  const handleView = () => setViewDetails(!viewDetails)
+
+  return (
+    <div>
+      <div>
+        <div>
+          {blog.title}
+          
+          <button onClick={handleView}>
+            view
+          </button>
+        </div>
+        {viewDetails && <div>
+          <div>{blog.url}</div>
+          <div>{blog.likes}</div>
+        </div>}
+        <div>
+         {blog.author}
+        </div>
+      </div>
+    </div>  
+  )
+}
 
 export default Blog
