50d803c5f74f494a3d14ef07eccd56c7f4ec57cc
Se Joon Park <sparky9043@gmail.com>
2025-07-21 22:05:39 +0900

    remove logging token to console

--- part4/bloglist/controller/login.js
+++ part4/bloglist/controller/login.js
@@ -31,8 +31,6 @@ loginRouter.post('/', async (request, response) => {
     }
   )
 
-  console.log(token)
-
   response
     .status(200)
     .send({ token, username: user.username, name: user.name })
