6f115affa57bb9636c0c52f71375f8e120edab13
Se Joon Park <sparky9043@gmail.com>
2025-07-21 18:03:45 +0900

    create test for logging in with right and wrong creds

--- part5/bloglist-testing/tests/blog_app.spec.js
+++ part5/bloglist-testing/tests/blog_app.spec.js
@@ -1,4 +1,5 @@
 const { test, expect, beforeEach, describe } = require('@playwright/test')
+const { loginWith } = require('./helper')
 
 describe('Blog app', () => {
   beforeEach(async ({ page, request }) => {
@@ -17,5 +18,20 @@ describe('Blog app', () => {
     await expect(page.getByText('log in to application')).toBeVisible()
     await expect(page.getByText('username')).toBeVisible()
     await expect(page.getByText('password')).toBeVisible()
+  })
+
+  describe('Login', () => {
+    test('succeeds with correct credentials', async ({ page }) => {
+      await loginWith(page, 'example', 'password123')
+
+      await expect(page.getByText('jonathan winters logged in')).toBeVisible()
+    })
+    
+    test('fails with wrong credentials', async ({ page }) => {
+      await loginWith(page, 'invaliduser', 'badpassword')
+
+      await expect(page.getByText('invalid username or password')).toBeVisible()
+    })
+
   })
 })
