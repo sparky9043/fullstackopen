8167f00637e24554e01918640ffa734199da7e4f
Se Joon Park <sparky9043@gmail.com>
2025-07-23 17:28:45 +0900

    create a filter component with boilerplate code

---
+++ part6/redux-anecdotes/src/components/Filter.jsx
@@ -0,0 +1,20 @@
+const Filter = () => {
+  const handleChange = (event) => {
+    console.log(event.target.value)
+  }
+
+  const style = {
+    marginBottom: 10
+  }
+
+  return (
+    <div style={style}>
+      filter
+      <input
+        onChange={handleChange}
+      />
+    </div>
+  )
+}
+
+export default Filter
