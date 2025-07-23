12b0238c8dbe79cdd9525342b1d8c6cd1277c084
Se Joon Park <sparky9043@gmail.com>
2025-07-23 16:31:47 +0900

    refactor anecdotes list into its own component

--- part6/redux-anecdotes/src/App.jsx
+++ part6/redux-anecdotes/src/App.jsx
@@ -1,31 +1,14 @@
 import { useSelector, useDispatch } from 'react-redux'
 import { vote } from './reducers/anecdoteReducer'
 import AnecdoteForm from './components/AnecdoteForm'
+import AnecdoteList from './components/AnecdoteList'
 
 const App = () => {
-  const anecdotes = useSelector(state => state)
-  const dispatch = useDispatch()
-
-  const handleVote = (id) => {
-    dispatch(vote(id))
-  }
-
-  const anecdotesSorted = [...anecdotes].sort((a, b) => b.votes - a.votes)
 
   return (
     <div>
       <h2>Anecdotes</h2>
-      {anecdotesSorted.map(anecdote =>
-        <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => handleVote(anecdote.id)}>vote</button>
-          </div>
-        </div>
-      )}
+      <AnecdoteList />
       <AnecdoteForm />
     </div>
   )
--- part6/redux-anecdotes/src/components/AnecdoteList.jsx
+++ part6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -1,5 +1,32 @@
-const AnecdoteList = () => {
+import { useDispatch, useSelector } from 'react-redux'
+import { vote } from '../reducers/anecdoteReducer'
 
+const AnecdoteList = () => {
+  const dispatch = useDispatch()
+  const anecdotes = useSelector(state => state)
+
+  const anecdotesSorted = [...anecdotes].sort((a, b) => b.votes - a.votes)
+
+  
+  const handleVote = (id) => {
+    dispatch(vote(id))
+  }
+
+  return (
+    <>
+      {anecdotesSorted.map(anecdote =>
+        <div key={anecdote.id}>
+          <div>
+            {anecdote.content}
+          </div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => handleVote(anecdote.id)}>vote</button>
+          </div>
+        </div>
+      )}
+    </>
+  )
 }
 
 export default AnecdoteList
