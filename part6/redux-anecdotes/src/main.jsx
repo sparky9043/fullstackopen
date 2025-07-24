e80c4d64422bf55375407c56dec86b2e14bcd6fb
Se Joon Park <sparky9043@gmail.com>
2025-07-24 11:09:22 +0900

    delete comment

--- part6/redux-anecdotes/src/main.jsx
+++ part6/redux-anecdotes/src/main.jsx
@@ -1,16 +1,10 @@
 import ReactDOM from 'react-dom/client'
-// import { createStore, combineReducers } from 'redux'
 import { configureStore } from '@reduxjs/toolkit'
 import { Provider } from 'react-redux'
 import App from './App'
 import anecdoteReducer from './reducers/anecdoteReducer'
 import filterReducer from './reducers/filterReducer'
 import notificationReducer from './reducers/notificationReducer'
-
-// const reducer = combineReducers({
-//   anecdotes: anecdoteReducer,
-//   filter: filterReducer
-// })
 
 const store = configureStore({
   reducer: {
