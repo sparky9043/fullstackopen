83dc4cec1b93a4101aebea3e2c90ffcbcb179a40
Se Joon Park <sparky9043@gmail.com>
2025-07-24 13:47:04 +0900

    remove comment in anecdote reducer

--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -8,9 +8,6 @@ const anecdoteSlice = createSlice({
     vote: (state, action) => {
       state.find(a => a.id === action.payload).votes++
     },
-    // createAnecdote: (state, action) => {
-    //   state.push(action.payload)
-    // },
     setAnecdotes: (state, action) => {
       return action.payload
     },
