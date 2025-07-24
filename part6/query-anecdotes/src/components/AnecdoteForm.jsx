7789d49b234987f09d06ecad91b45f553b3f3800
Se Joon Park <sparky9043@gmail.com>
2025-07-24 17:13:09 +0900

    add error handling for when content is too short

--- part6/query-anecdotes/src/components/AnecdoteForm.jsx
+++ part6/query-anecdotes/src/components/AnecdoteForm.jsx
@@ -1,7 +1,9 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query'
 import { createAnecdote } from '../services/anecdotes'
+import { useNotificationDispatch } from '../hooks/notificationHooks'
 
 const AnecdoteForm = () => {
+  const dispatch = useNotificationDispatch()
   const queryClient = useQueryClient()
 
   const newAnecdoteMutation = useMutation({
@@ -16,6 +18,15 @@ const AnecdoteForm = () => {
     event.preventDefault()
     const content = event.target.anecdote.value
     event.target.anecdote.value = ''
+    
+    if (content.length < 5) {
+      dispatch({ type: 'updateMessage', payload: 'too short anecdote, must have length 5 or more' })
+      setTimeout(() => {
+        dispatch({ type: 'removeMessage' })
+      }, 5000)
+      return;
+    }
+
     newAnecdoteMutation.mutate({ content, votes: 0 })
     console.log('new anecdote')
 }
