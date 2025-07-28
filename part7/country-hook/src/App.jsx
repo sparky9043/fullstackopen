38d205f90ee3b04d01f633eec30ad22c61a2b876
Se Joon Park <sparky9043@gmail.com>
2025-07-28 14:44:41 +0900

    refactor country into its own component

--- part7/country-hook/src/App.jsx
+++ part7/country-hook/src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import Country from './components/Country'
 import axios from 'axios'
 
 const useField = (type) => {
@@ -23,29 +24,6 @@ const useCountry = (name) => {
   return country
 }
 
-const Country = ({ country }) => {
-  if (!country) {
-    return null
-  }
-
-  if (!country.found) {
-    return (
-      <div>
-        not found...
-      </div>
-    )
-  }
-
-  return (
-    <div>
-      <h3>{country.data.name} </h3>
-      <div>capital {country.data.capital} </div>
-      <div>population {country.data.population}</div> 
-      <img src={country.data.flag} height='100' alt={`flag of ${country.data.name}`}/>  
-    </div>
-  )
-}
-
 const App = () => {
   const nameInput = useField('text')
   const [name, setName] = useState('')
@@ -60,7 +38,7 @@ const App = () => {
     <div>
       <form onSubmit={fetch}>
         <input {...nameInput} />
-        <button>find</button>
+        <button type="submit">find</button>
       </form>
 
       <Country country={country} />
