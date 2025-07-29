321247b7f6084e1d8185f33caf3e0762cb7fab53
Se Joon Park <sparky9043@gmail.com>
2025-07-29 16:06:10 +0900

    wrap notification context provider around main app

--- part7/blog-query/blog-frontend/src/main.jsx
+++ part7/blog-query/blog-frontend/src/main.jsx
@@ -3,13 +3,16 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { NotificationContextProvider } from './contexts/NotificationContext.jsx'
 
 const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <NotificationContextProvider>
+        <App />
+      </NotificationContextProvider>
     </QueryClientProvider>
   </StrictMode>,
 )
