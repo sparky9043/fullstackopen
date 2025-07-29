7b757d66255ae47a9fc924a8a6b6c53c6e6f3664
Se Joon Park <sparky9043@gmail.com>
2025-07-29 17:45:06 +0900

    refactor bloglist and create blog form to HomePage component

--- part7/blog-query/blog-frontend/src/App.jsx
+++ part7/blog-query/blog-frontend/src/App.jsx
@@ -31,74 +31,7 @@ const App = () => {
       notificationDispatch({ type: 'removeNotification' })
     }, 5000)
   }, [])
-
-  const createNewBlogMutation = useMutation({
-    mutationFn: blogService.create,
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ['blogs'] })
-    }
-  })
-
-  const deleteBlogMutation = useMutation({
-    mutationFn: blogService.deleteOne,
-    onSuccess: () => {
-      queryClient.invalidateQueries(['blogs'])
-    }
-  })
-
-  const likeBlogMutation = useMutation({
-    mutationFn: blogService.update,
-    onSuccess: () => {
-      queryClient.invalidateQueries(['blogs'])
-    }
-  })
-
-  const handleCreateBlog = (blogObject) => {
-    createNewBlogMutation.mutate(blogObject)
-    blogFormRef.current.setVisibility()
-  }
-
-  const queryClient = useQueryClient()
   
-  const result = useQuery({
-    queryKey: ['blogs'],
-    queryFn: blogService.getAll
-  })
-
-  if (result.isLoading) {
-    return <div>Waiting to load...</div>
-  }
-
-  const blogs = result.data
-
-  if (!blogs) {
-    return (
-      <div>Waiting to load...</div>
-    )
-  }
-
-
-  const handleDeleteBlog = (id) => {
-    deleteBlogMutation.mutate(id)
-    notificationDispatch({
-      type: 'updateNotification',
-      payload: 'post successfully deleted!',
-    })
-    setTimeout(() => {
-      notificationDispatch({ type: 'removeNotification' })
-    }, 5000)
-  }
-
-  const handleLike = async (newObject) => {
-    try {
-      likeBlogMutation.mutate(newObject)
-    } catch (exception) {
-      console.log(exception)
-    }
-  }
-
-  const sortedBlogs = [...blogs].sort((a, b) => b.likes - a.likes)
-
   return (
     <div>
       <h2>blogs</h2>
@@ -106,20 +39,6 @@ const App = () => {
       <Routes>
         <Route path='/' element={user ? <HomePage /> : <LoginForm /> } />
       </Routes>
-
-      <Toggleable buttonLabel='new blog' ref={blogFormRef}>
-        <BlogForm createBlog={handleCreateBlog} />
-      </Toggleable>
-
-      {/* {sortedBlogs.map((blog, index) =>
-        <Blog
-          key={blog.id}
-          blog={blog} 
-          likePost={handleLike}
-          deletePost={handleDeleteBlog}
-          user={user}
-        />
-      )} */}
     </div>
   )
 }
--- part7/blog-query/blog-frontend/src/components/HomePage.jsx
+++ part7/blog-query/blog-frontend/src/components/HomePage.jsx
@@ -1,10 +1,65 @@
 import { useContext } from 'react'
 import { CurrentUserContext } from '../contexts/CurrentUserContext'
 import { NotificationContext } from '../contexts/NotificationContext'
+import Toggleable from './Toggleable'
+import blogService from '../services/blogs'
+import BlogForm from './BlogForm'
+import { useRef } from 'react'
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
+import Blog from './Blog'
 
 const HomePage = () => {
   const [user, userDispatch] = useContext(CurrentUserContext)
   const [notification, notificationDispatch] = useContext(NotificationContext)
+
+  const blogFormRef = useRef()
+
+  const queryClient = useQueryClient()
+
+  const createNewBlogMutation = useMutation({
+    mutationFn: blogService.create,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['blogs'] })
+    }
+  })
+
+  const deleteBlogMutation = useMutation({
+    mutationFn: blogService.deleteOne,
+    onSuccess: () => {
+      queryClient.invalidateQueries(['blogs'])
+    }
+  })
+
+  const likeBlogMutation = useMutation({
+    mutationFn: blogService.update,
+    onSuccess: () => {
+      queryClient.invalidateQueries(['blogs'])
+    }
+  })
+
+  const handleDeleteBlog = (id) => {
+    deleteBlogMutation.mutate(id)
+    notificationDispatch({
+      type: 'updateNotification',
+      payload: 'post successfully deleted!',
+    })
+    setTimeout(() => {
+      notificationDispatch({ type: 'removeNotification' })
+    }, 5000)
+  }
+
+  const handleLike = async (newObject) => {
+    try {
+      likeBlogMutation.mutate(newObject)
+    } catch (exception) {
+      console.log(exception)
+    }
+  }
+
+  const handleCreateBlog = (blogObject) => {
+    createNewBlogMutation.mutate(blogObject)
+    blogFormRef.current.setVisibility()
+  }
 
   const handleLogout = () => {
     console.log('logged out successfully')
@@ -15,6 +70,27 @@ const HomePage = () => {
       notificationDispatch({ type: 'removeNotification' })
     }, 5000)
   }
+
+
+  const result = useQuery({
+    queryKey: ['blogs'],
+    queryFn: blogService.getAll
+  })
+
+  if (result.isLoading) {
+    return <div>Waiting to load...</div>
+  }
+
+  const blogs = result.data
+
+  if (!blogs) {
+    return (
+      <div>Waiting to load...</div>
+    )
+  }
+
+  const sortedBlogs = [...blogs].sort((a, b) => b.likes - a.likes)
+
   return (
     <div>
       <p>This is the homepage!</p>
@@ -24,6 +100,20 @@ const HomePage = () => {
           <button onClick={handleLogout}>logout</button>
         </span>
       </div>
+
+      <Toggleable buttonLabel='new blog' ref={blogFormRef}>
+        <BlogForm createBlog={handleCreateBlog} />
+      </Toggleable>
+
+      {sortedBlogs.map((blog, index) =>
+        <Blog
+          key={blog.id}
+          blog={blog} 
+          likePost={handleLike}
+          deletePost={handleDeleteBlog}
+          user={user}
+        />
+      )}
     </div>
 
   )
