ec09656907a61415881f924decebf44cda627acc
Se Joon Park <sparky9043@gmail.com>
2025-07-29 17:31:17 +0900

    add notification logic to homepage logout

--- part7/blog-query/blog-frontend/src/components/HomePage.jsx
+++ part7/blog-query/blog-frontend/src/components/HomePage.jsx
@@ -1,8 +1,10 @@
 import { useContext } from 'react'
 import { CurrentUserContext } from '../contexts/CurrentUserContext'
+import { NotificationContext } from '../contexts/NotificationContext'
 
 const HomePage = () => {
   const [user, userDispatch] = useContext(CurrentUserContext)
+  const [notification, notificationDispatch] = useContext(NotificationContext)
 
   const handleLogout = () => {
     console.log('logged out successfully')
