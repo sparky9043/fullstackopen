a57db3f918ebee88237c98fc81e417e9e3b0c46e
Se Joon Park <sparky9043@gmail.com>
2025-07-29 17:23:05 +0900

    wrap app around browser router

--- part7/blog-query/blog-frontend/src/main.jsx
+++ part7/blog-query/blog-frontend/src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { BrowserRouter as Router } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { NotificationContextProvider } from './contexts/NotificationContext.jsx'
 import { CurrentUserContextProvider } from './contexts/CurrentUserContext.jsx'
@@ -13,7 +14,9 @@ createRoot(document.getElementById('root')).render(
     <QueryClientProvider client={queryClient}>
       <CurrentUserContextProvider>
         <NotificationContextProvider>
-          <App />
+          <Router>
+            <App />
+          </Router>
         </NotificationContextProvider>
       </CurrentUserContextProvider>
     </QueryClientProvider>
