b29904607b30c176937445f7f95c1c5a8860ece8
Se Joon Park <sparky9043@gmail.com>
2025-07-29 20:11:40 +0900

    fetch users data via react query

--- part7/blog-query/blog-frontend/src/components/UsersPage.jsx
+++ part7/blog-query/blog-frontend/src/components/UsersPage.jsx
@@ -1,4 +1,23 @@
+import { useQuery } from '@tanstack/react-query'
+import userService from '../services/users'
+
 const UsersPage = () => {
+
+  const result = useQuery({
+    queryKey: ['users'],
+    queryFn: userService.getUsers
+  })
+
+  if ( result.isLoading ) {
+    return (
+      <p>Waiting to load users...</p>
+    )
+  }
+
+  const users = result.data
+
+  console.log(users)
+
   return (
     <div>
       users page
