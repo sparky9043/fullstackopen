deb50b26bc03363226f04f8ee38fa6ffc1fe7d54
Se Joon Park <sparky9043@gmail.com>
2025-07-30 14:51:40 +0900

    refactor parse arguments function into utils file

--- part9/bmiCalculator.ts
+++ part9/bmiCalculator.ts
@@ -1,26 +1,3 @@
-interface bmiBio {
-  height: number,
-  weight: number,
-}
-
-const parseArguments = (args: string[]): bmiBio => {
-  if (args.length < 4) throw new Error('Not enough arguments')
-  if (args.length > 4) throw new Error('Too many arguments')
-
-  const height = Number(args[2])
-  const weight = Number(args[3])
-
-  if (!isNaN(height) && !isNaN(weight)) {
-    return {
-      height,
-      weight,
-    }
-  } else {
-    throw new Error('You must enter a number')
-  }
-}
-
-
 const calculateBmi = (height: number, weight: number): string => {
 
   const heightInMeters = height / 100
@@ -36,10 +13,6 @@ const calculateBmi = (height: number, weight: number): string => {
     return 'Underweight';
   }
 }
-
-console.log(
-  parseArguments(process.argv)
-)
 
 // console.log(
   // calculateBmi(process.argv)
---
+++ part9/utils/parseArguments.ts
@@ -0,0 +1,23 @@
+interface bmiBio {
+  height: number,
+  weight: number,
+}
+
+const parseArguments = (args: string[]): bmiBio => {
+  if (args.length < 4) throw new Error('Not enough arguments')
+  if (args.length > 4) throw new Error('Too many arguments')
+
+  const height = Number(args[2])
+  const weight = Number(args[3])
+
+  if (!isNaN(height) && !isNaN(weight)) {
+    return {
+      height,
+      weight,
+    }
+  } else {
+    throw new Error('You must enter a number')
+  }
+}
+
+export default parseArguments
