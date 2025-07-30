4ebd2ca2010eba6d700fcdb9a1885d574ddcec43
Se Joon Park <sparky9043@gmail.com>
2025-07-30 14:14:32 +0900

    use if then conditionals to create bmi logic

--- part9/bmiCalculator.ts
+++ part9/bmiCalculator.ts
@@ -1,5 +1,16 @@
 const calculateBmi = (height: number, weight: number): string => {
-  return 'Normal Range'
+  const heightInMeters = height / 100
+  const bmi: number = weight / (heightInMeters * heightInMeters);
+
+  if (bmi >= 30) {
+    return 'Obese';
+  } else if (bmi >= 25) {
+    return 'Overweight';
+  } else if (bmi >= 18.5) {
+    return 'Normal Range';
+  } else {
+    return 'Underweight';
+  }
 }
 
-console.log(calculateBmi(180, 74))
+console.log(calculateBmi(173, 95))
