979ed2c490a4b29f61087066a83f49ee37915c97
Se Joon Park <sparky9043@gmail.com>
2025-07-31 11:47:15 +0900

    setup post request to exercises to receive correct daily exercises and target hours

--- part9/index.ts
+++ part9/index.ts
@@ -1,7 +1,9 @@
 import express from 'express';
 import calculateBmi from './bmiCalculator';
+import exerciseCalculator from './exerciseCalculator';
 
 const app = express();
+app.use(express.json());
 
 app.get('/hello', (_req, res) => {
   res.send('<h1>Hello Full Stack!</h1>');
@@ -21,6 +23,26 @@ app.get('/bmi', (req, res) => {
   } else {
     res.status(400).json({ error: 'malformatted parameters' });
   }
+});
+
+app.post('/exercises', (req, res) => {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
+  const daily_exercises: number[] = req.body.daily_exercises;
+
+  if (daily_exercises.length === 0 || !daily_exercises) {
+    console.log('wrong exercises');
+    return res.status(400).json({ error: 'malformatted parameter' });
+  }
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
+  const target: number = req.body.target;
+  if (!target || isNaN(Number(target))) {
+    console.log('wrong target');
+    return res.status(400).json({ error: 'malformatted parameter' });
+  }
+
+  const result = exerciseCalculator(daily_exercises, target);
+
+  return res.status(201).json(result);
 });
 
 const PORT: number = 3001;
