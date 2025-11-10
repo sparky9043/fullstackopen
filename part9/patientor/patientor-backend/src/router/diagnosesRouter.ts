64bb6600bba3a7e7409b4e28e7d5d4dbe7ed1b8d
Se Joon Park <sparky9043@gmail.com>
2025-11-10 16:06:20 +0900

    add resolveJsonModule to true in tsconfig and make sure diagnoses route returns all diagnose

--- part9/patientor/patientor-backend/src/router/diagnosesRouter.ts
+++ part9/patientor/patientor-backend/src/router/diagnosesRouter.ts
@@ -1,9 +1,12 @@
 import express from 'express';
+import diagnosesService from '../services/diagnosesService';
+import { Diagnosis } from '../types';
 
 const diagnosesRouter = express.Router();
 
 diagnosesRouter.get('/', (_req, res) => {
-  res.send('diagnoses retrieved');
+  const diagnoses: Diagnosis[] = diagnosesService.getDiagnoses();
+  res.json(diagnoses);
 });
 
 export default diagnosesRouter;
