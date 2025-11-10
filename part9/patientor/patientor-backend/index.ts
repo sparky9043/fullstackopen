9c70b81ebdce6fc9337bc0c31bf99b80057db3af
Se Joon Park <sparky9043@gmail.com>
2025-11-10 15:22:05 +0900

    install cors types

--- part9/patientor/patientor-backend/index.ts
+++ part9/patientor/patientor-backend/index.ts
@@ -3,16 +3,11 @@ import cors from 'cors';
 
 const app = express();
 app.use(express.json());
-// eslint-disable-next-line @typescript-eslint/no-unsafe-call
 app.use(cors());
 
 const PORT = 3001;
 
 app.get('/api/ping', (_req, res) => {
-  res.json({ success: 'route setup successfully' });
-});
-
-app.get('/api/patients', (_req, res) => {
   res.json({ success: 'route setup successfully' });
 });
 
