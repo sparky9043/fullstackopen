df7dfda61419d3ba718c6de9891c0ac7f4f6963c
Se Joon Park <sparky9043@gmail.com>
2025-11-10 15:31:22 +0900

    refactor routes into their own folder

--- part9/patientor/patientor-backend/src/app.ts
+++ part9/patientor/patientor-backend/src/app.ts
@@ -1,8 +1,11 @@
 import express from 'express';
 import cors from 'cors';
+import pingRouter from './router/ping';
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+app.use('/ping', pingRouter);
 
 export default app;
--- part9/patientor/patientor-backend/src/index.ts
+++ part9/patientor/patientor-backend/src/index.ts
@@ -2,10 +2,6 @@ import app from './app';
 
 const PORT = 3001;
 
-app.get('/api/ping', (_req, res) => {
-  res.json({ success: 'route setup successfully' });
-});
-
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
