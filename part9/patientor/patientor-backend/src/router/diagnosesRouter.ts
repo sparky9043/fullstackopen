641445f2d361543f180d06a18a432c558d2ecfc8
Se Joon Park <sparky9043@gmail.com>
2025-11-10 16:07:12 +0900

    type get response for diagnoses

--- part9/patientor/patientor-backend/src/router/diagnosesRouter.ts
+++ part9/patientor/patientor-backend/src/router/diagnosesRouter.ts
@@ -1,10 +1,10 @@
-import express from 'express';
+import express, { Response } from 'express';
 import diagnosesService from '../services/diagnosesService';
 import { Diagnosis } from '../types';
 
 const diagnosesRouter = express.Router();
 
-diagnosesRouter.get('/', (_req, res) => {
+diagnosesRouter.get('/', (_req, res: Response<Diagnosis[]>) => {
   const diagnoses: Diagnosis[] = diagnosesService.getDiagnoses();
   res.json(diagnoses);
 });
