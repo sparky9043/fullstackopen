a757117fe1c04f534e0fb96591cd5c675ba87eb3
Se Joon Park <sparky9043@gmail.com>
2025-11-11 18:51:50 +0900

    create string and date checker and parse date and ssn functions

--- part9/patientor/patientor-backend/src/utils.ts
+++ part9/patientor/patientor-backend/src/utils.ts
@@ -1,9 +1,37 @@
 import { NewPatient } from './types';
 import { v1 as uuid } from 'uuid';
 
-// const isString = (input: unknown) => {
-//   return typeof input === 'string' || input instanceof String;
-// };
+const isString = (input: unknown): input is string => {
+  return typeof input === 'string' || input instanceof String;
+};
+
+const parseName = (name: unknown) => {
+  if (!name || !isString(name)) {
+    throw new Error('invalid or missing name: ' + name);
+  }
+
+  return name;
+};
+
+const isDate = (date: string): boolean => {
+  return Boolean(Date.parse(date))
+};
+
+const parseDate = (date: unknown): string => {
+  if (!date || !isString(date) || !isDate(date)) {
+    throw new Error('invalid or missing date: ' + date);
+  }
+
+  return date;
+};
+
+const parseSSN = (input: unknown): string => {
+  if (!input || !isString(input)) {
+    throw new Error('invalid or missing ssn' + input);
+  }
+
+  return input;
+};
 
 const toNewPatient = (object: unknown): NewPatient => {
   console.log(object);
