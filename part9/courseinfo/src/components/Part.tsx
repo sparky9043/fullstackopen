9d5e6571c60b2a6385a07a4fe112ebf754e97107
Se Joon Park <sparky9043@gmail.com>
2025-11-18 15:25:22 +0900

    make sure each part is rendered properly via switch case

--- part9/courseinfo/src/components/Part.tsx
+++ part9/courseinfo/src/components/Part.tsx
@@ -6,11 +6,41 @@ interface PartProps {
 
 const Part = (props: PartProps) => {
   console.log(props);
+  const courseParts = (): React.ReactNode => {
+    switch (props.course.kind) {
+      case 'basic':
+        return <p>
+          <em>
+            {props.course.description}
+          </em>
+        </p>
+      case 'group':
+        return <p>
+          <em>
+            project exercises {props.course.groupProjectCount}
+          </em>
+        </p>
+      case 'background':
+        return <div>
+          <em>
+            {props.course.description}
+          </em>
+          <p>
+            submit to&nbsp;
+            <span>
+              {props.course.backgroundMaterial}
+            </span>
+          </p>
+        </div>
+    }
+  }
+
   return (
     <div>
       <strong>
         {props.course.name} {props.course.exerciseCount}
       </strong>
+      {courseParts()}
     </div>
   )
 };
