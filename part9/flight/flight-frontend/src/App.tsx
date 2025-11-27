e779f1aaa1d0d329648649c1fa4ad8bd0c7f1d2e
Se Joon Park <sparky9043@gmail.com>
2025-11-27 17:11:46 +0900

    add general type to diaries and use axios to fetch diaries data from backend

--- part9/flight/flight-frontend/src/App.tsx
+++ part9/flight/flight-frontend/src/App.tsx
@@ -1,8 +1,27 @@
+import { useEffect, useState } from 'react'
 import './App.css'
+import axios from 'axios'
+import type { Diary } from './types'
+const baseUrl = 'http://localhost:3000/api/diaries'
 
 const App = () => {
+  const [diaries, setDiaries] = useState<Diary[] | null>(null);
+
+  useEffect(() => {
+    axios.get(baseUrl)
+      .then(response => setDiaries(response.data));
+  }, []);
+
+  if (!diaries) {
+    return <p>Loading...</p>
+  }
+
+  console.log(diaries);
+
   return (
-    <p>Hello</p>
+    <div>
+      {diaries.map(diary => <li key={diary.id}>{diary.weather}</li>)}
+    </div>
   )
 }
 
