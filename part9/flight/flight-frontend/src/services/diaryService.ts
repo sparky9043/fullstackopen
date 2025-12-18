fef92ccfc3ec85f8c1608738ed1fd40fd905dd55
Se Joon Park <sparky9043@gmail.com>
2025-12-18 13:54:44 +0900

    make createDiary function that accepts new entry and returns saved entry

--- part9/flight/flight-frontend/src/services/diaryService.ts
+++ part9/flight/flight-frontend/src/services/diaryService.ts
@@ -1,9 +1,14 @@
 import axios from 'axios';
-import type { Diary } from '../types';
+import type { Diary, NewDiary } from '../types';
 
 const baseUrl = 'http://localhost:3000/api/diaries';
 
 export const getAllDiaries = async () => {
   return axios.get<Diary[]>(baseUrl)
+    .then(response => response.data);
+}
+
+export const createDiary = async (newDiary: NewDiary) => {
+  return axios.post<Diary>(baseUrl, newDiary)
     .then(response => response.data);
 }
--- part9/flight/flight-frontend/src/types.ts
+++ part9/flight/flight-frontend/src/types.ts
@@ -1,6 +1,8 @@
 export interface Diary {
-  id: number,
-  date: string,
-  weather: string,
-  visibility: string,
+  id: number;
+  date: string;
+  weather: string;
+  visibility: string;
 }
+
+export type NewDiary = Omit<Diary, 'id'>;
