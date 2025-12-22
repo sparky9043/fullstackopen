aa8702b07fa60ded90d97a603bc983e52cee3e32
Se Joon Park <sparky9043@gmail.com>
2025-12-22 12:17:02 +0900

    fetch patient using id with axios

--- part9/patientor/patientor-frontend/src/components/PatientDetailPage/PatientDetail.tsx
+++ part9/patientor/patientor-frontend/src/components/PatientDetailPage/PatientDetail.tsx
@@ -1,6 +1,27 @@
+import axios from 'axios';
+import { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { apiBaseUrl } from '../../constants';
+import { Typography } from '@mui/material';
+
 const PatientDetail = () => {
+  const { id: patientId } = useParams();
+
+  useEffect(() => {
+    const fetchPatientById = async () => {
+      const response = await axios.get(`${apiBaseUrl}/patients/${patientId}`);
+      console.log(response.data);
+    };
+
+    void fetchPatientById();
+  }, [patientId]);
+
   return (
-    <p>Hello</p>
+    <div>
+      <Typography variant="h4" style={{ marginBottom: "0.5em" }}>
+        Hello
+      </Typography>
+    </div>
   );
 };
 
