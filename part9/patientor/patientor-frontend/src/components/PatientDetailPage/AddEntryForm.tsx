88637437c7af5ec784e6dbfc97c48176205a2e1f
Se Joon Park <sparky9043@gmail.com>
2025-12-26 15:53:36 +0900

    add inputs for description date specialist and diagnosis codes

--- part9/patientor/patientor-frontend/src/components/PatientDetailPage/AddEntryForm.tsx
+++ part9/patientor/patientor-frontend/src/components/PatientDetailPage/AddEntryForm.tsx
@@ -1,8 +1,17 @@
-import { Button, Typography } from '@mui/material';
+import { Button, Input, InputLabel, List, ListItem, Typography } from '@mui/material';
+import { useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 const AddEntryForm = () => {
+  const { id: patientId } = useParams();
+  const [entryDescription, setEntryDescription] = useState<string>('');
+  const [entryDate, setEntryDate] = useState<string>('');
+  const [specialist, setSpecialist] = useState<string>('');
+  const [diagnosisCodes, setDiagnosisCodes] = useState<string>('');
+  console.log(patientId);
+
   const handleSubmitEntry = (event: React.SyntheticEvent) => {
-    console.log(event);
+    console.log(entryDate, entryDescription);
     event.preventDefault();
   };
 
@@ -26,6 +35,50 @@ const AddEntryForm = () => {
           entry
         </Typography>
       </div>
+      <List style={{ listStyle: 'none' }}>
+        <ListItem>
+          <InputLabel htmlFor='entryDate'>date</InputLabel>
+          <Input  
+            value={entryDate}
+            id='entryDate'
+            type='date'
+            onChange={(e) => setEntryDate(e.currentTarget.value)}
+          />
+        </ListItem>
+        <ListItem>
+          <InputLabel>
+            description
+          </InputLabel>
+          <Input
+            value={entryDescription}
+            onChange={(e) => setEntryDescription(e.currentTarget.value)}
+            id='entryDescription'
+            type='text'
+          />
+        </ListItem>
+        <ListItem>
+          <InputLabel>
+            specialist
+          </InputLabel>
+          <Input
+            value={specialist}
+            onChange={(e) => setSpecialist(e.currentTarget.value)}
+            id='specialist'
+            type='text'
+          />
+        </ListItem>
+        <ListItem>
+          <InputLabel>
+            diagnosis codes
+          </InputLabel>
+          <Input
+            value={diagnosisCodes}
+            onChange={(e) => setDiagnosisCodes(e.currentTarget.value)}
+            id='diagnosisCodes'
+            type='text'
+          />
+        </ListItem>
+      </List>
 
       <Button style={buttonStyle} type='submit' variant='contained'>
         submit
