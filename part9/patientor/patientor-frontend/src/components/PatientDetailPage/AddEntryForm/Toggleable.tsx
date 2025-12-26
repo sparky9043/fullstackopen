f5559e3f4aa27a9a695047c6c688d3bb58495079
Se Joon Park <sparky9043@gmail.com>
2025-12-26 15:28:36 +0900

    refactor toggle function

--- part9/patientor/patientor-frontend/src/components/PatientDetailPage/AddEntryForm/Toggleable.tsx
+++ part9/patientor/patientor-frontend/src/components/PatientDetailPage/AddEntryForm/Toggleable.tsx
@@ -9,14 +9,16 @@ interface ToggleableProps {
 const Toggleable = (props: ToggleableProps) => {
   const [toggle, setToggle] = useState<boolean>(false);
 
+  const handleToggle = (): void => setToggle(prev => !prev);
+
   return (
     <div>
-      <div style={{ display: !toggle ? 'block' : 'none' }}>
-        <Button variant='outlined' onClick={() => setToggle(prev => !prev)}>{!props.text ? 'show': props.text}</Button>
+      <div style={{ display: toggle ? 'none' : 'block' }}>
+        <Button variant='outlined' onClick={handleToggle}>{!props.text ? 'show': props.text}</Button>
       </div>
       <div style={{ display: toggle ? 'block' : 'none' }}>
         {props.children}
-        <Button variant='outlined' onClick={() => setToggle(prev => !prev)}>cancel</Button>
+        <Button variant='outlined' onClick={handleToggle}>cancel</Button>
       </div>
     </div>
   );
